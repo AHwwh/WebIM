a809eb2c30466cf02fe40052f742608a6556e20d
yqqyang <Administrator@v_yqqyang-PC0.tencent.com>
2017-03-31 12:59:39 +0800

    Revert "离线push同步"
    This reverts commit 770798d70b72eebc079866ebbb4dc5be5778ab3c.

--- js/switch_chat_obj.js
+++ js/switch_chat_obj.js
@@ -1,5 +1,4 @@
 //更新最近会话的未读消息数
-
 function updateSessDiv(sess_type, to_id, name, unread_msg_count) {
     var badgeDiv = document.getElementById("badgeDiv_" + to_id);
     if (badgeDiv && unread_msg_count > 0) {
@@ -20,7 +19,6 @@ function updateSessDiv(sess_type, to_id, name, unread_msg_count) {
 }
 
 //新增一条最近会话
-
 function addSess(sess_type, to_id, name, face_url, unread_msg_count, sesslist, addPositonType) {
     var sessDivId = "sessDiv_" + to_id;
     var sessDiv = document.getElementById(sessDivId);
@@ -73,7 +71,6 @@ function addSess(sess_type, to_id, name, face_url, unread_msg_count, sesslist, a
 }
 
 //切换好友或群组聊天对象
-
 function onSelSess(sess_type, to_id) {
     if (selToID != null) {
 
@@ -155,7 +152,6 @@ function onSelSess(sess_type, to_id) {
 }
 
 //删除会话
-
 function deleteSessDiv(sess_type, to_id) {
     var sessDiv = document.getElementById("sessDiv_" + to_id);
     sessDiv && sessDiv.parentNode.removeChild(sessDiv);
@@ -163,7 +159,6 @@ function deleteSessDiv(sess_type, to_id) {
 
 
 //更新最近会话的名字
-
 function updateSessNameDiv(sess_type, to_id, newName) {
 
     var nameDivId = "nameDiv_" + to_id;
@@ -177,7 +172,6 @@ function updateSessNameDiv(sess_type, to_id, newName) {
 }
 
 //更新最近会话的头像
-
 function updateSessImageDiv(sess_type, to_id, newImageUrl) {
     if (!newImageUrl) {
         return;
@@ -185,7 +179,7 @@ function updateSessImageDiv(sess_type, to_id, newImageUrl) {
     var faceImageId = "faceImg_" + to_id;
     var faceImage = document.getElementById(faceImageId);
     if (faceImage) {
-        faceImage.innerHTML = webim.Tool.formatText2Html(newImageUrl);
+        nameDiv.innerHTML = webim.Tool.formatText2Html(newImageUrl);
     }
 }
 
