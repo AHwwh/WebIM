8234f949f7e3159a069dbf34bc844907b5063fbc
neallin <neallzq@gmail.com>
2017-08-09 20:06:00 +0800

    多终端登录处理
    多终端登录处理

--- js/logout/logout.js
+++ js/logout/logout.js
@@ -25,3 +25,12 @@ function onKickedEventCall(){
     webim.Log.error("其他地方登录，被T了");
     document.getElementById("webim_demo").style.display = "none";
 }
+
+
+
+//多终端登录被T
+function onMultipleDeviceKickedOut() {
+    webim.Log.error("多终端登录，被T了");
+    document.getElementById("webim_demo").style.display = "none";
+}
+
